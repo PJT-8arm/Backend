26ec3d20a95c5dfae4341aeed3301cda3dbcd022
geniushee <h1116jeon@gmail.com>
2024-02-28 14:59:25 +0900

    init

---
+++ src/main/java/com/example/be8arm/Be8armApplication.java
@@ -0,0 +1,13 @@
+package com.example.be8arm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Be8armApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Be8armApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/be8arm/Be8armApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be8arm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be8armApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
