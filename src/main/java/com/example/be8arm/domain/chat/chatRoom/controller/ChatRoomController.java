498dae12f032bf52ff447915b9c9463439773e36
taehoon <xogns2134@gmail.com>
2024-03-05 16:35:13 +0900

    refactor: exitChatRoom을 GetMapping에서 DeleteMapping으로 변경

--- src/main/java/com/example/be8arm/domain/chat/chatRoom/controller/ChatRoomController.java
+++ src/main/java/com/example/be8arm/domain/chat/chatRoom/controller/ChatRoomController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -101,7 +102,7 @@ public class ChatRoomController {
 
 	}
 
-	@GetMapping("/exit/{chatRoomId}")
+	@DeleteMapping("/exit/{chatRoomId}")
 	public ResponseEntity<?> exitChatRoom(
 		@PathVariable Long chatRoomId,
 		@AuthenticationPrincipal UserDetails user) {
