d0d08643db9cf260cc87f53593ecfd0cc56f26f4
seo-sang-ho <rocter2049@naver.com>
2024-03-06 16:59:00 +0900

    fix : 모집 글 데이터 null 값 입력되는 버그 수정

--- src/main/java/com/example/be8arm/domain/recruitment/recruitment/controller/RecruitmentController.java
+++ src/main/java/com/example/be8arm/domain/recruitment/recruitment/controller/RecruitmentController.java
@@ -1,5 +1,6 @@
 package com.example.be8arm.domain.recruitment.recruitment.controller;
 
+import com.example.be8arm.domain.member.member.entity.Member;
 import com.example.be8arm.domain.recruitment.recruitment.dto.RecruitmentCreateRequestDto;
 import com.example.be8arm.domain.recruitment.recruitment.dto.RecruitmentCreateResponseDto;
 import com.example.be8arm.domain.recruitment.recruitment.dto.RecruitmentListDetailResponseDto;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.attribute.UserPrincipal;
@@ -24,10 +26,10 @@ public class RecruitmentController {
 
     @PostMapping("/write") // 작성
     public ResponseEntity<RecruitmentCreateResponseDto> recruitmentAdd(
-            @AuthenticationPrincipal UserPrincipal user,
-            @ModelAttribute RecruitmentCreateRequestDto recruitmentCreateRequestDto){
+            @AuthenticationPrincipal Member user,
+            @RequestBody RecruitmentCreateRequestDto recruitmentCreateRequestDto){
 
-        RecruitmentCreateResponseDto recruitmentCreateResponseDto = recruitmentService.addRecruitment(user.getMember(), recruitmentCreateRequestDto);
+        RecruitmentCreateResponseDto recruitmentCreateResponseDto = recruitmentService.addRecruitment(user, recruitmentCreateRequestDto);
 
         return ResponseEntity.ok(recruitmentCreateResponseDto);
     }
--- src/main/java/com/example/be8arm/domain/recruitment/recruitment/dto/RecruitmentListDto.java
+++ src/main/java/com/example/be8arm/domain/recruitment/recruitment/dto/RecruitmentListDto.java
@@ -1,9 +1,16 @@
 package com.example.be8arm.domain.recruitment.recruitment.dto;
 
 import com.example.be8arm.domain.member.member.entity.Member;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
 public class RecruitmentListDto { // 전체 글 조회 dto
     private Member member;
 
