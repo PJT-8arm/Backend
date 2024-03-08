e8b7f323c209251a17a34a05349b66550906bbe1
geniushee <h1116jeon@gmail.com>
2024-03-08 13:41:35 +0900

    comment : 회원정보 수정 todolist 작성

---
+++ src/main/java/com/example/be8arm/domain/member/mypage/controller/MypageController.java
@@ -0,0 +1,20 @@
+package com.example.be8arm.domain.member.mypage.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/mypage")
+public class MypageController {
+
+	//todo 보류. GET 회원정보 수정 전 비밀번호 재확인 페이지 - 데이터=username
+	//todo 보류. POST 회원정보 수정 전 비밀번호 재확인 페이지 -
+	//Todo GET 회원정보 수정 페이지 - 데이터 member정보
+	//todo POST 회원정보 수정 페이지 - 데이터 member정보
+
+}
