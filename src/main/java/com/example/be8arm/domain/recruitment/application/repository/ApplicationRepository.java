5c7f2f3720acd2dcddd4c9f3c1e92d53bb05d5e0
geniushee <129508219+geniushee@users.noreply.github.com>
2024-03-27 18:37:44 +0900

    main merge : cors에 하드코딩 (#88)
    * 86/fix/get profile (#87)
    
    * fix : tracnsactional 어노테이션 수정
    
    * feat : 나의 약속 목록 조회
    
    * fix : cors config
    
    ---------
    
    Co-authored-by: YUBEENLIM <148295292+YUBEENLIM@users.noreply.github.com>
    Co-authored-by: seo-sang-ho <125337743+seo-sang-ho@users.noreply.github.com>
    Co-authored-by: taehoon <xogns2134@gmail.com>
    Co-authored-by: jinyoung <131987204+jinyoung121636@users.noreply.github.com>

--- src/main/java/com/example/be8arm/domain/recruitment/application/repository/ApplicationRepository.java
+++ src/main/java/com/example/be8arm/domain/recruitment/application/repository/ApplicationRepository.java
@@ -1,8 +1,12 @@
 package com.example.be8arm.domain.recruitment.application.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.be8arm.domain.member.member.entity.Member;
 import com.example.be8arm.domain.recruitment.application.entity.Application;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
+	Page<Application> findByWriter(Member writer, Pageable pageable);
 }
