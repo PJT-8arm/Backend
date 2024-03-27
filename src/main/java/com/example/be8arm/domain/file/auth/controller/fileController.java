fecfb6f18e3459622674755dc95d1a3d271ffcda
taehoon <xogns2134@gmail.com>
2024-03-27 13:10:58 +0900

    refactor: dto 방식으로 수정

--- src/main/java/com/example/be8arm/domain/file/auth/controller/fileController.java
+++ src/main/java/com/example/be8arm/domain/file/auth/controller/fileController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.be8arm.domain.file.auth.dto.PreSignedUrlDto;
 import com.example.be8arm.domain.file.auth.service.AwsAuthService;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,16 +17,17 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/file")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "File", description = "FileApi")
 public class fileController {
 
 	private final AwsAuthService awsAuthService;
 
 	@GetMapping("/upload/url")
 	public ResponseEntity<PreSignedUrlDto> getPreSignedUrl(
-		@RequestParam(value = "prefix") String prefix,
-		@RequestParam(value = "filename", required = false) String filename
+		@RequestParam String prefix,
+		@RequestParam(required = false) String fileName
 	) {
-		String preSignedUrl = awsAuthService.getPreSignedUrl(prefix, filename);
+		String preSignedUrl = awsAuthService.getPreSignedUrl(prefix, fileName);
 
 		return ResponseEntity.ok(new PreSignedUrlDto(preSignedUrl));
 	}
