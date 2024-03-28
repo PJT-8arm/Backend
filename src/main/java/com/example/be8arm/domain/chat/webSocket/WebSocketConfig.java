604bae570c8f5342cecbcf486f5cbb5a4576fa65
taehoon <xogns2134@gmail.com>
2024-03-28 13:07:04 +0900

    fix: websocket 프론트 url 하드코딩

--- src/main/java/com/example/be8arm/domain/chat/webSocket/WebSocketConfig.java
+++ src/main/java/com/example/be8arm/domain/chat/webSocket/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws")
-			.setAllowedOrigins(webAppUrl)
+			.setAllowedOrigins("http://api.arm.genj.me")
 			.withSockJS();
 	}
 
